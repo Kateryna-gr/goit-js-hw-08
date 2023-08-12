e7a7d4fa627aab2ea072136b500578866b7bbf74
Kateryna-gr <128936446+Kateryna-gr@users.noreply.github.com>
2023-08-12 23:19:42 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import throttle from 'lodash.throttle';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on('timeupdate', throttle(() => {
+  player.getCurrentTime().then(function (time) {
+    console.log('time:', time);
+    localStorage.setItem('videoplayer-current-time', time);
+  });
+}, 1000));
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
+
+// document.addEventListener(
+//     "click", throttle(() => {
+//       console.log("Scroll handler call every 300ms");
+//     }, 300));
