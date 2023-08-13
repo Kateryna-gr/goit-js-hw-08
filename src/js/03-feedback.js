db1d0f8f9a5b8f1a6f62a048ef376904fb2446b2
Kateryna-gr <128936446+Kateryna-gr@users.noreply.github.com>
2023-08-13 15:57:06 +0300

    task3-fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,17 +5,33 @@ const form = document.querySelector('.feedback-form');
 const email = document.querySelector('.js-email');
 const message = document.querySelector('.js-message');
 
-email.value = localStorage.getItem('feedback-form-email');
-message.value = localStorage.getItem('feedback-form-message');
+const STORAGE_KEY = 'feedback-form-state';
+
+let storageData = {
+  storEmail: '',
+  storMessage: '',
+};
+
+let storage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+console.log(storage);
+
+if (storage) {
+  email.value = storage.storEmail;
+  message.value = storage.storMessage;
+}
 
 form.addEventListener('input', throttle(handlerInput, 500));
 
 function handlerInput() {
   console.log('email:', email.value);
-  localStorage.setItem('feedback-form-email', email.value);
-
+  storageData.storEmail = email.value;
+ 
   console.log('message:', message.value);
-  localStorage.setItem('feedback-form-message', message.value);
+  storageData.storMessage = message.value;
+ 
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(storageData));
+
+  console.log(localStorage.getItem(STORAGE_KEY));
 }
 
 form.addEventListener('submit', handlerForm);
@@ -24,11 +40,10 @@ function handlerForm(event) {
   event.preventDefault();
 
   if (!email.value || !message.value) {
-    console.log('Незаповнені поля');
+    alert('Незаповнені поля!');
   } else {
     console.log({ Email: email.value, Message: message.value });
     event.currentTarget.reset();
-    localStorage.removeItem('feedback-form-email');
-    localStorage.removeItem('feedback-form-message');
+    localStorage.removeItem(STORAGE_KEY);
   }
 }
